d78457e7326c95d0001224731aa3367a9bd376d2
Phaedon V <faival@protonmail.com>
2021-01-11 13:53:52 +0100

    tobefixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+
+puts "hello friend"
+
+puts "creating tasks"
+
+p task1 = Task.new(
+  title: "shopping",
+  details: "do the shopping",
+  completed: false
+  )
+task1.save
+
+p task2 = Task.create!(
+  title: "catwalk",
+  details: "walk the cat to the park",
+  completed: true
+  )
+task2.save
+
+puts "Goodbye friend"
