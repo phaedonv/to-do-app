2f790d81b89e068b4f8a85ff00b31f73d146c277
Phaedon Valasiadis <faival@protonmail.com>
2021-09-14 13:21:56 +0200

    working with many users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,23 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-
+   # @task = Task.find params[:id]
   end
 
   def new
-    @task = Task.new
+    #@task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       redirect_to tasks_path, notice: 'Task was successfully created.'
@@ -41,6 +43,11 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    redirect_to tasks_path, notice: "Not Authorized to Edit!" if @task.nil?
+  end
+
   private
 
   def set_task
@@ -48,6 +55,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :user_id, :completed)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_163009) do
+ActiveRecord::Schema.define(version: 2021_09_13_173212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_09_08_163009) do
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
